4096fbe316c5c8a0c6504a71c9af848c2b2a4bbc
flf2ko <noobfriend@gmail.com>
2013-02-14 00:02:04 +0800

    upload flow finish V1.0

--- upload.php
+++ upload.php
@@ -14,7 +14,7 @@
 	
 	date_default_timezone_set('Asia/Taipei');
 	$datetime = date ("Y/m/d H:i:s");
-	echo $datetime.'<br>';
+	//echo $datetime.'<br>';
 	
 	if (!$_SESSION['account']){            // 使用者未登入，導回login.php
 		header ("Location:login.php") ;
@@ -38,31 +38,42 @@
 		$outputfile = $problem_dir.'\\output.txt';
 		move_uploaded_file($_FILES['upload']['tmp_name'], $upfile);
 		
-		// if (!file_exists($logfile)){
-			// $fp = fopen($logfile, 'w');
-			// fclose($fp);
-		// }
-	
-		$return = 1;
+
+		$status = '';
+		$return = -1;
+		$num = (int)$_POST['problem_num'][2].$_POST['problem_num'][3].$_POST['problem_num'][4];
 		
 		//編譯.cpp檔
 		//$command = 'g++ '.$upfile.' -o '.$exefile.' -enable-auto-import 2> '.$logfile;
-		$command = 'g++ '.$upfile.' -o '.$exefile.' 2> '.$compile_logfile;
-		system($command,$return);
-		
-		//如果成功編譯出.exe檔 執行.exe 輸入測資為$testfile 標準輸出轉向至$outputfile 標準錯誤轉向至$run_logfile
-		if (file_exists($exefile)){
-			$command = $exefile.' < '.$testfile.' > '.$outputfile.' 2> '.$run_logfile;
+		if (file_exists($upfile)){
+			$command = 'g++ '.$upfile.' -o '.$exefile.' 2> '.$compile_logfile;
 			system($command,$return);
+			if ($return == 0){	//如果成功編譯出.exe檔 執行.exe 輸入測資為$testfile 標準輸出轉向至$outputfile 標準錯誤轉向至$run_logfile
+				$command = $exefile.' < '.$testfile.' > '.$outputfile.' 2> '.$run__logfile;
+				system($command,$return);
+				if ($return == 0){  //如果執行成功  比對結果
+					$command = 'python judge.py '.$_SESSION['account'].' '.$_POST['problem_num'];
+					$score = exec($command,$return);
+				} else {
+					//runtime error
+					$status = 'runtime error';
+				}
+			} else {
+				//compile error
+				$status = 'compile error';
+			}
 		} else {
-			//compile error
+			//upload error
+			$status = 'upload error';
 		}
+		$query = "SELECT s_id FROM student WHERE account = '".$_SESSION['account']."'" ;
+		$id = mysql_query($query);
+		$fetch_id = mysql_fetch_row($id);
+		echo $_POST['problem_num'][4];
+		$insert = "INSERT INTO pd_score(s_id, p_id, status, time, score) 
+			VALUES ('$fetch_id[0]', '$num', '$status', '$datetime', '$score')" ;
+		$success = mysql_query($insert);
 		
-		$command = 'python judge.py '.$_SESSION['account'].' '.$_POST['problem_num'];
-		system($command,$return);
-		
-		echo "Hello, ".$_SESSION['account']."<br>";
-		echo 'file upload success';
 	}
 	else if (!isset($_FILES['upload']) and !isset($_POST['textarea'])){
 		echo 'no upload';
