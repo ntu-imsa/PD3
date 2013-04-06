8bfa8317a798ace0a669f7886666a0a92a6a2e6d
flf2ko <noobfriend@gmail.com>
2013-04-07 01:50:19 +0800

    solve the judge.py score problem
    get the total score from database now

--- labjudge.py
+++ labjudge.py
@@ -1,22 +1,27 @@
 import os
 import sys
+import MySQLdb
 
 if __name__ == '__main__':
-    #python judge.py  s_id  p_id
+    
     hw_path = './student/' + sys.argv[1] + '/' + sys.argv[2] + '/answer/'
     judge_path = './judgement/' + sys.argv[2] + '/'
     outputfile = open(hw_path + 'output.txt', 'r')
     answerfile = open(judge_path + 'answer.txt', 'r')
-
+    conn = MySQLdb.connect(host="127.0.0.1",user="root",passwd="",db="pd course")  
+    cursor = conn.cursor()   
+	
+    n = cursor.execute("SELECT total_score FROM lab_hw WHERE lab_id = '" + sys.argv[3] + "'")
+    row = cursor.fetchall() 
+    print row[0][0]
     total_score = 0
     test_num = int(answerfile.readline().strip())
     for i in range(test_num):
         a = answerfile.readline().strip()
         o = outputfile.readline().strip()
         if a == o:
-            total_score += 92 / test_num
+            total_score += row[0][0] / test_num
     outfile = open(hw_path + 'score.txt', 'w')
-    #outfile.write("total_score is : " + str(total_score))
     outfile.write(str(total_score))
     outputfile.close()
     answerfile.close()
