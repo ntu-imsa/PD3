505d3c4ce70d36e28732fb1480e5bc9824115e45
shouko <h999342@gmail.com>
2014-05-17 08:38:46 +0800

    修正 lougout.php 行為
    因送出header之前不得有其他內容，且本頁之html內容為不必要

--- logout.php
+++ logout.php
@@ -1,15 +1,5 @@
 <?php
-	ob_start();
 	session_start() ;
-	unset($_SESSION['account']) ;
-	unset($_SESSION['number']) ;
+	session_destroy();
+	header ("Location:index.php");
 ?>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-</head>
-<body>
-<?php header ("Location:index.php");
-?>
-</body>
-</html>
