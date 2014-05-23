4e64dc2bdf053298d55bd0ee9363cb367fcbf902
Shouko <h999342@gmail.com>
2014-05-23 20:15:54 +0800

    更新score_board.php的ajax部分

--- score_board.php
+++ score_board.php
@@ -1,11 +1,18 @@
 <!doctype html>
 <html>
 <head>
+<meta charset="utf-8">
 	<script src="js/jquery.min.js" type="text/javascript"></script>
 	<script type="text/javascript">
 	$(document).ready(function()
     {
-        $("#myTable").tablesorter();
+			var content;
+			$.get("score_board_ajax.php", function(data){
+				for(var i = 0; i < data.length; i++){
+				            content += "index：" + i + "; value： " + data[i]['p_id'];
+				}
+				alert(content);
+			});
     }
 	);
     </script>
