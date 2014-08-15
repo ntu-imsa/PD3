8e21422bf9fef18f2ed206699aaeb72ffbaea9ed
Shouko <h999342@gmail.com>
2014-08-16 03:19:46 +0800

    Add announce to SQL, add pd3_structure.sql which contains no sample data

--- SQL files/pd3.sql
+++ SQL files/pd3.sql
@@ -3,22 +3,38 @@
 -- http://www.phpmyadmin.net
 --
 -- 主機: 127.0.0.1
--- 產生日期: 2014 年 07 月 28 日 12:22
+-- 產生日期: 2014 年 08 月 15 日 21:19
 -- 伺服器版本: 5.5.27
 -- PHP 版本: 5.4.7
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
 -- 資料庫: `pd3`
 --
+
+-- --------------------------------------------------------
+
+--
+-- 表的結構 `announce`
+--
+
+CREATE TABLE IF NOT EXISTS `announce` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `content` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- 轉存資料表中的資料 `announce`
+--
+
+INSERT INTO `announce` (`id`, `date`, `content`) VALUES
+(1, '2014-02-19', '<p>To submit Homework 1:</p>\r\n	<ul>\r\n		<li>For the PDF for Problems 1 to 3 and the CPP for Problem 4, submit to "PD14-1a"</li>\r\n		<li>For the CPP for Problem 5 (bonus), submit to "PD14-1b"</li>\r\n	</ul>'),
+(2, '2014-02-22', '<p>Because we will manual check your homework 1, so we didn''t upload testing data to the PDOGS. </p>\r\n	<p>So you will get 0 point and runtime error even your code is totally correct!</p>'),
+(3, '2014-02-24', '<p>To submit Homework 2:</p>\r\n	<ul>\r\n		<li>For the PDF for Problems 1 to 2 and the CPP for Problem 3, submit to "PD14-02a"</li>\r\n		<li>For the CPP for Problem 4 (bonus), submit to "PD14-02b"</li>\r\n		<li>Download <a href="/PD/problem/PD14-2-template.cpp">template</a> that repeatedly reads the testing data for Problem 3</li>\r\n	</ul>');
 
 -- --------------------------------------------------------
 
@@ -42,8 +58,8 @@ CREATE TABLE IF NOT EXISTS `group` (
 --
 
 INSERT INTO `group` (`group_num`, `group_name`, `rank`, `best_score`, `worst_score`, `total_score`) VALUES
-(1, '資管帥哥哥裁判隊', 0, 0, 0, 0),
-(2, '測試隊1號', 0, 0, 0, 0),
+(1, 'ADMIN', 0, 0, 0, 0),
+(2, '[Judge] 帥哥哥', 0, 0, 0, 0),
 (3, '測試隊2號', 0, 0, 0, 0);
 
 -- --------------------------------------------------------
@@ -128,15 +144,17 @@ CREATE TABLE IF NOT EXISTS `pd_hw` (
 --
 
 INSERT INTO `pd_hw` (`p_id`, `submit_code`, `submit_pdf`, `data_number`, `type`, `total_score`, `deadline`) VALUES
-('ProblemA', 1, 0, 2, 0, 8, '3014-05-15 23:59:59'),
-('ProblemB', 1, 0, 2, 0, 8, '3014-05-15 23:59:59'),
-('ProblemC', 1, 0, 2, 0, 8, '3014-05-15 23:59:59'),
-('ProblemD', 1, 0, 2, 0, 8, '3014-05-15 23:59:59'),
-('ProblemE', 1, 0, 2, 0, 8, '3014-05-15 23:59:59'),
-('ProblemF', 1, 0, 2, 0, 8, '3014-05-15 23:59:59'),
-('ProblemG', 1, 0, 2, 0, 8, '3014-05-15 23:59:59'),
-('P_STACKSZ', 1, 0, 2, 0, 8, '3014-05-15 23:59:59'),
-('P_TESTPRO', 1, 0, 2, 0, 8, '3014-05-15 23:59:59');
+('Problem_A', 1, 0, 2, 0, 8, '2014-12-23 23:59:59'),
+('Problem_B', 1, 0, 2, 0, 8, '2014-12-23 23:59:59'),
+('Problem_C', 1, 0, 2, 0, 8, '2014-12-23 23:59:59'),
+('Problem_D', 1, 0, 2, 0, 8, '2014-12-23 23:59:59'),
+('Problem_E', 1, 0, 2, 0, 8, '2014-12-23 23:59:59'),
+('Problem_F', 1, 0, 2, 1, 8, '2014-12-23 23:59:59'),
+('Problem_G', 1, 0, 2, 0, 8, '2014-12-23 23:59:59'),
+('Problem_H', 1, 0, 2, 0, 8, '2014-12-23 23:59:59'),
+('Problem_I', 1, 0, 2, 3, 8, '2014-12-23 23:59:59'),
+('Problem_J', 1, 0, 2, 3, 8, '2014-08-28 00:00:00'),
+('P_TEST', 1, 0, 1, 3, 100, '2015-01-01 01:01:01');
 
 -- --------------------------------------------------------
 
@@ -160,26 +178,20 @@ CREATE TABLE IF NOT EXISTS `pd_score` (
 --
 
 INSERT INTO `pd_score` (`s_id`, `p_id`, `status`, `time`, `exec_time`, `score`, `result`) VALUES
-(72, 'ProblemA', 'Time limit exceed', '2014-05-24 18:08:04', 2, 0, '0,0,'),
-(72, 'ProblemA', 'Time limit exceed', '2014-05-24 18:08:10', 2, 0, '0,0,'),
-(72, 'ProblemA', 'Accepted', '2014-05-24 20:19:57', 0, 8, '4,4,'),
-(72, 'ProblemC', 'Time limit exceed', '2014-05-24 20:12:33', 2, 0, '0,0,'),
-(72, 'ProblemC', 'Time limit exceed', '2014-05-24 20:18:33', 2, 0, '0,0,'),
-(72, 'ProblemC', 'Time limit exceed', '2014-05-24 20:18:46', 2, 0, '0,0,'),
-(72, 'ProblemC', 'Time limit exceed', '2014-05-24 20:19:10', 2, 0, '0,0,'),
-(72, 'ProblemC', 'Accepted', '2014-05-24 20:19:34', 0, 8, '4,4,'),
-(72, 'P_STACKSZ', 'Accepted', '2014-05-24 20:11:55', 0, 8, '4,4,'),
-(72, 'P_TESTPRO', 'Time limit exceed', '2014-05-24 18:06:45', 1, 4, '4,0,'),
-(72, 'P_TESTPRO', 'Time limit exceed', '2014-05-24 18:07:03', 1, 4, '4,0,'),
-(75, 'ProblemA', 'Accepted', '2014-05-24 16:35:44', 0, 8, '4,4,'),
-(75, 'ProblemB', 'Accepted', '2014-05-24 16:35:48', 0, 8, '4,4,'),
-(75, 'ProblemC', 'Accepted', '2014-05-24 16:35:52', 0, 8, '4,4,'),
-(75, 'ProblemD', 'Accepted', '2014-05-24 16:35:56', 0, 8, '4,4,'),
-(75, 'ProblemE', 'Accepted', '2014-05-24 16:35:59', 0, 8, '4,4,'),
-(75, 'ProblemF', 'Accepted', '2014-05-24 16:36:03', 0, 8, '4,4,'),
-(75, 'ProblemG', 'Accepted', '2014-05-24 16:36:07', 0, 8, '4,4,'),
-(75, 'P_STACKSZ', 'Accepted', '2014-05-24 16:36:10', 0, 8, '4,4,'),
-(75, 'P_TESTPRO', 'Accepted', '2014-05-24 16:36:14', 0, 8, '4,4,');
+(72, 'Problem_A', 'Accepted', '2014-08-10 00:38:32', 0, 8, '4,4,'),
+(72, 'Problem_B', 'Time limit exceed', '2014-08-10 00:38:46', 6, 0, '0,0,'),
+(72, 'Problem_C', 'Wrong answer', '2014-08-10 00:44:07', 0, 4, '4,0,'),
+(72, 'Problem_D', 'Accepted', '2014-08-10 00:48:35', 0, 8, '3,5,'),
+(72, 'Problem_E', 'Wrong answer', '2014-08-13 16:16:54', 0, 5, '0,5,'),
+(72, 'Problem_F', 'Accepted', '2014-08-15 12:06:28', 0, 8, '4,4,'),
+(72, 'Problem_G', 'Accepted', '2014-08-10 00:50:56', 0, 8, '3,5,'),
+(72, 'Problem_H', 'Accepted', '2014-08-10 00:51:18', 0, 8, '3,5,'),
+(72, 'Problem_I', 'Wrong answer', '2014-08-15 21:18:56', 0, 2, '2,0,'),
+(72, 'P_TEST', 'Wrong answer', '2014-08-13 17:04:48', 0, 0, '0,'),
+(74, 'Problem_A', 'Wrong answer', '2014-08-16 01:14:27', 0, 0, '0,0,'),
+(74, 'Problem_C', 'Time limit exceed', '2014-08-16 01:15:02', 5.5, 4, '4,0,'),
+(74, 'Problem_C', 'Time limit exceed', '2014-08-16 01:18:55', 2, 0, '0,0,'),
+(74, 'Problem_F', 'Accepted', '2014-08-16 01:17:22', 0, 8, '4,4,');
 
 -- --------------------------------------------------------
 
@@ -246,17 +258,16 @@ CREATE TABLE IF NOT EXISTS `student` (
   `group_num` int(11) NOT NULL,
   `type` tinyint(4) NOT NULL,
   PRIMARY KEY (`s_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=76 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=79 ;
 
 --
 -- 轉存資料表中的資料 `student`
 --
 
 INSERT INTO `student` (`s_id`, `account`, `password`, `group_num`, `type`) VALUES
-(72, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1, 1),
-(74, 'b02705021', '7cf0199933ee85d80ecda21f3b5c35ed', 2, 1),
-(75, 'b02705001', '138cd22d4458437c9b134ac412767d7b', 3, 1);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+(72, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1, 0),
+(74, 'b02705021', '71cdda0264a89b5556c7a6f505f69c78', 2, 0),
+(75, 'b02705001', '138cd22d4458437c9b134ac412767d7b', 3, 1),
+(76, '23432', '726784b1a8c22c54054674ef3bdb4153', 0, 1),
+(77, '333', '310dcbbf4cce62f762a2aaa148d556bd', 0, 1),
+(78, '444', '550a141f12de6341fba65b0ad0433500', 0, 1);
